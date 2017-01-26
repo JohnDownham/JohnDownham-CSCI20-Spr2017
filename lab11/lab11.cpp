67880e5ed7e6887aac1b871ac07acc8523c944b2
John Downham <jdownham002@student.butte.edu>
2017-01-26 22:51:14 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,9 @@
+/* start with a grid of roughly 50 numbers in order from least to greatest
+have a score box that when the program starts is set to a value of 10
+the program at start is to pick a random number on the grid as the 0 value
+nest the user can pick a number with thier mouse clicked
+if the number is lower then 0 value the program will say "higher" and subtract 1 point from the score
+if the number is higher then 0 value the program will say "lower" and subtract 1 point from the score
+if the 0 value is chosen and clicked the program will say "congratulations" and give the option to restart the program
+if you fail to guess 0 value in 10 tries (your score is set to 0) the program will highlight the correct number and say "please try again"\
+*/ 
